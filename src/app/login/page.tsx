83d4b32acea7c76e707670dc2468f11cbf377f4d
Yuri <104223310+Yuriko-Otorii@users.noreply.github.com>
2023-07-07 15:21:41 -0700

    Add react-hook-form and validation to ligin page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,6 +1,16 @@
+"use client";
+
 import Link from "next/link";
+import { useLoginValidation } from "../utils/auth/validations";
+import { loginData } from "../types/auth.type";
 
 const Login = () => {
+  const { register, handleSubmit, errors, isSubmitting, isDirty, isValid } = useLoginValidation()
+
+  const onSubmit = (user: loginData) => {
+
+  }
+
   return (
     <div className="relative overflow-hidden">
       <div className="w-[28rem] h-[28rem] rounded-full bg-[#4289A7] absolute top-[0%] left-[50%] -translate-x-1/2 -translate-y-1/2"></div>
@@ -10,15 +20,19 @@ const Login = () => {
       <div className="relative flex justify-center w-screen h-screen">
         <form 
           className="absolute top-[52%] left-[50%] -translate-x-1/2 -translate-y-1/2 z-50 w-[70%] bg-white/[.935] p-5 pt-7 rounded-lg drop-shadow-md"
-          
+          onSubmit={handleSubmit(onSubmit)}        
         >
           <div className="relative mb-4" data-te-input-wrapper-init>
             <input
               type="text"
-              className="peer block min-h-[auto] w-full pl-1 mb-[1.5rem] rounded border-b border-gray-500 bg-transparent py-[0.32rem] leading-[1.8] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
+              {...register("email")}
+              className={`peer block min-h-[auto] w-full pl-1 rounded border-b border-gray-500 bg-transparent py-[0.32rem] leading-[1.8] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0 ${!(errors.email?.message) && "mb-[1.5rem]"}`}
               id="exampleFormControlInput2"
               placeholder="Email address"
             />
+            {errors.email?.message &&
+              <p className="mb-[1.5rem] text-red-500">{errors.email?.message}</p> 
+            }
             <label
               htmlFor="exampleFormControlInput2"
               className="absolute left-0 pl-0.5 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm"
@@ -29,10 +43,14 @@ const Login = () => {
           <div className="relative mb-[3rem]" data-te-input-wrapper-init>
             <input
               type="password"
-              className="peer block min-h-[auto] w-full pl-1 rounded border-b border-gray-500 bg-transparent py-[0.32rem] leading-[1.8] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
+              {...register("password")}
+              className={`peer block min-h-[auto] w-full pl-1 rounded border-b border-gray-500 bg-transparent py-[0.32rem] leading-[1.8] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0 ${!(errors.password?.message) && "mb-[1.5rem]"}`}
               id="exampleFormControlInput22"
               placeholder="Password"
             />
+            {errors.password?.message &&
+              <p className="mb-[1.5rem] text-red-500">{errors.password?.message}</p> 
+            }
             <label
               htmlFor="exampleFormControlInput22"
               className="absolute left-0 pl-0.5 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm"
@@ -43,8 +61,11 @@ const Login = () => {
 
           <div className="text-center lg:text-left">
             <button
-              type="button"
-              className="inline-block rounded bg-[#4289A7] px-7 pb-2.5 pt-3 mb-3 text-sm font-medium uppercase leading-normal text-white transition duration-150 ease-in-out hover:bg-primary-600 focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
+              type="submit"
+              disabled={!isDirty || !isValid || isSubmitting}
+              className={`inline-block rounded bg-[#4289A7] px-7 pb-2.5 pt-3 mb-2 text-sm font-medium uppercase leading-normal text-white transition duration-150 ease-in-out hover:bg-primary-600 focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] disabled:opacity-25 ${
+                isSubmitting && "loading"
+              }`}
               data-te-ripple-init
               data-te-ripple-color="light"
             >
--- src/app/utils/auth/validations.ts
+++ src/app/utils/auth/validations.ts
@@ -4,7 +4,6 @@ import { yupResolver } from "@hookform/resolvers/yup";
 import * as Yup from "yup";
 
 export const useSignupValidation = () => {
-
   const validationSchema = Yup.object().shape({
     username: Yup.string().required("Username is required"),
     email: Yup
@@ -16,7 +15,25 @@ export const useSignupValidation = () => {
     confirmPassword: Yup.string().required("Confirm password is required"),
   });
 
-  
+  const formOptions = { resolver: yupResolver(validationSchema) };
+  const { register, handleSubmit, formState: { errors, isSubmitting, isDirty, isValid } } = useForm(formOptions);
+
+  return { register, handleSubmit, errors, isSubmitting, isDirty, isValid }
+} 
+
+export const useLoginValidation = () => {
+  const validationSchema = Yup.object().shape({
+    email: Yup
+      .string()
+      .lowercase()
+      .email("Wrong email format")
+      .required("Email is required"),
+    password: Yup
+      .string()
+      .required("Password is required")
+      .min(8, "Password must be at least 8 characters"),
+  });
+
   const formOptions = { resolver: yupResolver(validationSchema) };
   const { register, handleSubmit, formState: { errors, isSubmitting, isDirty, isValid } } = useForm(formOptions);
 
@@ -32,5 +49,4 @@ export const passwordSchema = Yup
       /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*(),.?":{}|<>])/.test(value!)
   )
   .min(8, "Password must be at least 8 characters")
-  .max(10, "Password must be up to 10 characters")
   .required("Password is required");
